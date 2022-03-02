ea3df4cb445a0c4196dacef31d210627bea18a89
Isnainy Nasrullah <nain.daftar@gmail.com>
2022-03-02 21:36:30 +0700

    Tambah generator Post Type dan Taxonomy

---
+++ src/YDO_Post_Type.php
@@ -0,0 +1,67 @@
+<?php 
+namespace Yukdiorder\Helper ;
+
+class YDO_Post_Type{
+	
+	public function __construct($nama, $args=null)
+	{
+		$this->nama = $nama;
+		$this->args = $args;
+		$this->labels= $labels = array(
+			'name'                  =>  ucfirst($this->nama) ,
+			'singular_name'         =>  ucfirst($this->nama) ,
+			'menu_name'             =>  ucfirst($this->nama) ,
+			'name_admin_bar'        =>  ucfirst($this->nama) ,
+			'archives'              =>  'Item' . ucfirst($this->nama) ,
+			'attributes'            =>  'Item Attributes', 'text_domain' ,
+			'parent_item_colon'     =>  'Parent Item:', 'text_domain' ,
+			'all_items'             =>  'Semua '. ucfirst($this->nama) ,
+			'add_new_item'          =>  'Tambah '. $this->nama .'Baru' ,
+			'add_new'               =>  'Tambah Baru' ,
+			'new_item'              =>  'Item Baru' ,
+			'edit_item'             =>  'Edit Item',  
+			'update_item'           =>  'Update Item' ,
+			'view_item'             =>  'Lihat Item', 
+			'view_items'            =>  'Lihat Items', 
+			'search_items'          =>  'Cari '. $this->nama, 
+			'not_found'             =>  'Tidak ditemukan',
+			'not_found_in_trash'    =>  'Not found in Trash', 
+			'featured_image'        =>  'Featured Image', 
+			'set_featured_image'    =>  'Set featured image', 
+			'remove_featured_image' =>  'Remove featured image',
+			'use_featured_image'    =>  'Use as featured image', 
+			'insert_into_item'      =>  'Tambahkan ke item', 
+			'uploaded_to_this_item' =>  'unggah item ini', 
+			'items_list'            =>  'Items list', 
+			'items_list_navigation' =>  'Items list navigation', 
+			'filter_items_list'     =>  'Filter items list', 
+		);
+		if ($args == null) {
+			$this->args = $args = array(
+				'label'                 => ucfirst($this->nama),
+				'description'           => 'type post'. $this->nama,
+				'labels'                => $labels,
+				'supports'              => array( 'title', 'editor' ),
+				'taxonomies'            => array( 'category', 'post_tag' ),
+				'hierarchical'          => false,
+				'public'                => true,
+				'show_ui'               => true,
+				'show_in_menu'          => true,
+				'menu_position'         => 5,
+				'show_in_admin_bar'     => true,
+				'show_in_nav_menus'     => true,
+				'can_export'            => true,
+				'has_archive'           => true,
+				'exclude_from_search'   => false,
+				'publicly_queryable'    => true,
+				'capability_type'       => 'page',
+			);
+		}
+	}
+
+	public function init(){
+		register_post_type($this->nama, $this->args);
+	}
+	
+}
+?>
---
+++ src/YDO_Taxonomy.php
@@ -0,0 +1,55 @@
+<?php 
+namespace Yukdiorder\Helper ;
+
+class YDO_Taxonomy
+{
+    // Register Custom Taxonomy
+    public function __construct($nama, $args = null)
+    {
+        $this->nama = $nama;
+        $this->labels = array(
+            'name'                       => ucfirst($this->nama),
+            'singular_name'              => ucfirst($this->nama),
+            'menu_name'                  => ucfirst($this->nama),
+            'all_items'                  => __('All Items', 'text_domain'),
+            'parent_item'                => __('Parent Item', 'text_domain'),
+            'parent_item_colon'          => __('Parent Item:', 'text_domain'),
+            'new_item_name'              => __('New ' . ucfirst($this->nama)),
+            'add_new_item'               => __('Add New ' . ucfirst($this->nama)),
+            'edit_item'                  => __('Edit ' . ucfirst($this->nama)),
+            'update_item'                => __('Update ' . ucfirst($this->nama)),
+            'view_item'                  => __('View ' . ucfirst($this->nama)),
+            'separate_items_with_commas' => __('Separate ' . ucfirst($this->nama) . ' with commas', 'text_domain'),
+            'add_or_remove_items'        => __('Add or remove ' . $this->nama, 'text_domain'),
+            'choose_from_most_used'      => __('Choose from the most used', 'text_domain'),
+            'popular_items'              => __('Popular ' . ucfirst($this->nama), 'text_domain'),
+            'search_items'               => __('Search ' . ucfirst($this->nama), 'text_domain'),
+            'not_found'                  => __('Not Found', 'text_domain'),
+            'no_terms'                   => __('No ' . ucfirst($this->nama), 'text_domain'),
+            'items_list'                 => __(ucfirst($this->nama) . ' list', 'text_domain'),
+            'items_list_navigation'      => __(ucfirst($this->nama) . 'list navigation', 'text_domain'),
+        );
+
+        if(is_null($args)){
+            $this->args = array(
+                'labels'                     => $this->labels,
+                'hierarchical'               => false,
+                'public'                     => true,
+                'show_ui'                    => true,
+                'show_admin_column'          => true,
+                'show_in_nav_menus'          => true,
+                'show_tagcloud'              => true,
+            );
+
+        }
+        
+        
+    }
+	public function init(){
+		// register_post_type($this->nama, $this->args);
+		register_taxonomy($this->nama, array('post'), $this->args);
+
+	}
+}
+
+?>
