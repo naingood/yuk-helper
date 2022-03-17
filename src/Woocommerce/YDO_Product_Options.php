103af0de5ad68bb2c403b56c8def0a259b3e0d95
Isnainy Nasrullah <nain.daftar@gmail.com>
2022-03-17 23:49:26 +0700

    tambah YDO_Admin_Notice

--- src/Woocommerce/YDO_Product_Options.php
+++ src/Woocommerce/YDO_Product_Options.php
@@ -1,7 +1,7 @@
 <?php 
 namespace Yukdiorder\Helper\Woocommerce ;
 
-class YDO_Admin_Product_Options {
+class YDO_Product_Options {
     
     public function __construct($hook = null, $tipe= null, $select = null, $id = null, $label = null, $placeholder = null ){
         if($hook == null ) {
---
+++ src/YDO_Admin_Notice.php
@@ -0,0 +1,39 @@
+<?php 
+namespace Yukdiorder\Helper ;
+
+class YDO_Admin_Notice {
+    public $pesan ; 
+
+    public function __construct($pesan, $type = null ){
+        if (is_null($type)) {
+            $this->type = 'notice-success' ;
+        }
+        switch ($type) {
+            case 'warning':
+                $this->type = 'notice-warning' ;
+                break;
+            case 'info':
+                $this->type = 'notice-info' ;
+                break;
+            case 'danger':
+                $this->type = 'notice-danger' ;
+                break; 
+            default:
+                $this->type = 'notice-success' ;
+                break;
+        }
+        $this->pesan = $pesan ;
+        $this->class = "notice $this->type is-dismissible";
+        
+    }
+
+    public function init(){
+        ?>
+        <div class="<?php echo $this->class ?>">
+            <p><?php echo $this->pesan ?></p>
+        </div>
+        <?php
+    }
+}
+
+?>
